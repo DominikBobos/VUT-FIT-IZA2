8306daaa942380854590f2cc551c3bfbf748ac78
Dominik Bobos <bobos.dominik@gmail.com>
2020-05-13 02:41:05 +0200

    maybe finally something useful

---
+++ MedPlanner/MedPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MedPlanner
+//
+//  Created by Dominik Boboš on 13.5.20.
+//  Copyright © 2020 Dominik Boboš. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
